6bdab747b71dcc03bb6e357bbc7467df5e35803a
Weslley Almeida <weslleyalmeida6@gmail.com>
2022-06-11 08:28:39 -0300

    Prettier

--- servidor-api/config/express.js
+++ servidor-api/config/express.js
@@ -1,16 +1,19 @@
 /* Código simplório, apenas para fornecer o serviço para a aplicação */
 
-var express = require('express')
-    ,app = express()
-    ,routes = require('../app/routes')
-    ,path =  require('path')
-    ,bodyParser = require('body-parser');
+var express = require("express"),
+  app = express(),
+  routes = require("../app/routes"),
+  path = require("path"),
+  bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
