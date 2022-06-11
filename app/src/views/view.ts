e3ab8bec561e0059dbef8fbeef8e252b23aa49e2
Weslley Almeida <weslleyalmeida6@gmail.com>
2022-06-11 06:54:01 -0300

    Curso 03 - Aula 01

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,28 +1,29 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
+  protected elemento: HTMLElement;
+  private escapar = false;
 
-    protected elemento: HTMLElement;
-    private escapar = false;
-
-    constructor(seletor: string, escapar?: boolean) {
-        const elemento = document.querySelector(seletor);
-        if (elemento) {
-            this.elemento = elemento as HTMLElement;
-        } else {
-            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
-        }
-        if (escapar) {
-            this.escapar = escapar;
-        }
+  constructor(seletor: string, escapar?: boolean) {
+    const elemento = document.querySelector(seletor);
+    if (elemento) {
+      this.elemento = elemento as HTMLElement;
+    } else {
+      throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
     }
-
-    public update(model: T): void {
-        let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
-        this.elemento.innerHTML = template;
+    if (escapar) {
+      this.escapar = escapar;
     }
+  }
 
-    protected abstract template(model: T): string;
+  @logarTempoDeExecucao()
+  public update(model: T): void {
+    let template = this.template(model);
+    if (this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+    }
+    this.elemento.innerHTML = template;
+  }
+
+  protected abstract template(model: T): string;
 }
