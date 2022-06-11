8667458df8902c3fe762749e1aa8e70eb72dcb8d
Weslley Almeida <weslleyalmeida6@gmail.com>
2022-06-11 08:33:25 -0300

    Update packages

--- servidor-api/config/express.js
+++ servidor-api/config/express.js
@@ -2,11 +2,9 @@
 
 var express = require("express"),
   app = express(),
-  routes = require("../app/routes"),
-  path = require("path"),
-  bodyParser = require("body-parser");
+  routes = require("../app/routes");
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
